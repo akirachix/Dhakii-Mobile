15b493115e89af1f09be4e4fb9b1133ab9c54cba
Shalom-uwamahoro <shalomuwamahoro@gmail.com>
2024-09-25 14:40:35 +0300

    resolved all conflicts successfully.

--- app/src/main/java/com/akirachix/mamamindtrial/api/ApiService.kt
+++ app/src/main/java/com/akirachix/mamamindtrial/api/ApiService.kt
@@ -8,15 +8,14 @@ import retrofit2.http.Path
 import retrofit2.http.Query
 
 interface ApiService {
-<<<<<<< HEAD
+
     @GET("api/careguides/")
     suspend fun getCareGuideArticles(@Query("category") category: String): List<CareGuideArticle>
 
     @GET("api/careguides/{id}")
     suspend fun getArticleDetails(@Path("id") id: Int): CareGuideArticle
 
-=======
->>>>>>> bef1f98eb6944153bd050f474211d112eab46e64
+
     @GET("api/mothers/")
     fun getMothersList(): Call<List<MotherDetail>>
 
