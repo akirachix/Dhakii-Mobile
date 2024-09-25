4120b0ce044c61e645a5d9e7f8727f0b2beb69bd
shallo-nairat <shallonairat@gmail.com>
2024-09-25 19:44:49 +0300

    Made changes on Single Sign on

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -75,6 +75,8 @@ dependencies {
     implementation ("com.github.bumptech.glide:glide:4.12.0")
     annotationProcessor ("com.github.bumptech.glide:compiler:4.12.0")
     implementation ("androidx.swiperefreshlayout:swiperefreshlayout:1.1.0")
+    implementation ("com.google.firebase:firebase-auth:latest_version")
+    implementation ("com.google.android.gms:play-services-auth:latest_version")
     
     // Kotlin Coroutines
     implementation(libs.kotlinx.coroutines.android)
